fe3762d523d5d7db39d97f0eb212570ebed3a3a4
M Haidar Hanif <git@mhaidarhanif.com>
2022-10-02 23:02:13 +0700

    feat: add new question with POST request

--- rest-api/index.js
+++ rest-api/index.js
@@ -1,11 +1,15 @@
 const questionsElement = document.getElementById("questions");
+const questionFormElement = document.getElementById("question-form");
 
 const renderQuestions = (questions) => {
   console.log({ questions });
 
   questionsElement.innerHTML = questions
     .map((question) => {
-      return `<div>${JSON.stringify(question)}</div>`;
+      return `<div>
+        <h2>${question.title}</h2>
+        <p>${question.description}</p>
+      </div>`;
     })
     .join("");
 };
@@ -18,5 +22,27 @@ const fetchQuestions = async () => {
 
   renderQuestions(questions);
 };
+
+const addNewQuestion = async (event) => {
+  event.preventDefault();
+
+  const newQuestion = {
+    title: "What is REST API?",
+    description: "REST API looks much better to handle backend communication.",
+  };
+
+  const response = await fetch(
+    "https://api.kontenbase.com/query/api/v1/29f479eb-b571-4de1-978b-10ac1755d57c/questions",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newQuestion),
+    }
+  );
+
+  fetchQuestions();
+};
+
+questionFormElement.addEventListener("submit", addNewQuestion);
 
 fetchQuestions();
